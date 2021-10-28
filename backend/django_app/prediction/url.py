c2ad1b8248716b7c17c4c96b9488ba56fe8e6c87
rzhu1999 <rzhu08448@gmail.com>
2021-10-28 15:16:17 -0700

    Backend Update: Created views for content-based recommendation

--- backend/django_app/prediction/url.py
+++ backend/django_app/prediction/url.py
@@ -3,6 +3,5 @@ from django.urls.resolvers import URLPattern
 import prediction.views as views
 
 urlpatterns = [
-    path('add/', views.api_add, name = 'api_add'),
-    path('add_values/', views.Add_Values.as_view(), name = 'api_add_values'),
+    path("predict/", views.simlar_recommend.as_view(), name="simlar_recommend"),
 ]
--- backend/django_app/prediction/views.py
+++ backend/django_app/prediction/views.py
@@ -1,10 +1,41 @@
+from django.shortcuts import render
 from django.http import response
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
+import numpy as np
+import pandas as pd
+import pymysql
+import os
 
 from . import content_based as cb
+
+
+class simlar_recommend(APIView):
+    def post(self, request, format=None):
+        host = os.environ.get("AWS_RDS_HOST")
+        username = os.environ.get("AWS_RDS_USER")
+        password = os.environ.get("AWS_RDS_PASSWORD")
+        dbname = "predictiondb"
+
+        connection = pymysql.connect(
+            host=host, user=username, password=password, database=dbname
+        )
+
+        metadata = pd.read_sql_query("SELECT * from Metabase", connection)
+
+        indices, matrix = cb.calculate_cos_sim_matrix(metadata)
+        result = cb.get_similar_recommendations(
+            title=request.data["title"],
+            n=request.data["num"],
+            metadata=metadata,
+            cosine_sim=matrix,
+            indices=indices,
+        )
+
+        return Response(result, status=200)
+
 
 # # Function based view to add numbers
 # @api_view(["GET", "POST"])
@@ -33,4 +64,3 @@ from . import content_based as cb
 #             sum += data[key]
 #         response_dict = {"sum": sum}
 #         return Response(response_dict, status=status.HTTP_201_CREATED)
-

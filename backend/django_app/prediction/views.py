dbbe0664b3a93375c5e3b10d591927e7e7a03ea6
rzhu1999 <rzhu08448@gmail.com>
2021-10-28 10:30:50 -0700

    Updated views.py

--- backend/django_app/prediction/views.py
+++ backend/django_app/prediction/views.py
@@ -4,31 +4,33 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-# Function based view to add numbers
-@api_view(["GET", "POST"])
-def api_add(request):
-    sum = 0
-    response_dict = []
-    if request.method == "GET":
-        # Do nothing for now
-        pass
-    elif request.method == "POST":
-        # Add the numbers
-        data = request.data
-        for key in data:
-            sum += data[key]
-        response_dict = {"sum": sum}
-    return Response(response_dict, status=status.HTTP_201_CREATED)
+from . import content_based as cb
 
-# Class based view to add numbers
-class Add_Values(APIView):
-    def post(self, request, format=None):
-        sum = 0
-        # Add the numbers
-       # Add the numbers
-        data = request.data
-        for key in data:
-            sum += data[key]
-        response_dict = {"sum": sum}
-        return Response(response_dict, status=status.HTTP_201_CREATED)
+# # Function based view to add numbers
+# @api_view(["GET", "POST"])
+# def api_add(request):
+#     sum = 0
+#     response_dict = []
+#     if request.method == "GET":
+#         # Do nothing for now
+#         pass
+#     elif request.method == "POST":
+#         # Add the numbers
+#         data = request.data
+#         for key in data:
+#             sum += data[key]
+#         response_dict = {"sum": sum}
+#     return Response(response_dict, status=status.HTTP_201_CREATED)
+
+# # Class based view to add numbers
+# class Add_Values(APIView):
+#     def post(self, request, format=None):
+#         sum = 0
+#         # Add the numbers
+#        # Add the numbers
+#         data = request.data
+#         for key in data:
+#             sum += data[key]
+#         response_dict = {"sum": sum}
+#         return Response(response_dict, status=status.HTTP_201_CREATED)
 

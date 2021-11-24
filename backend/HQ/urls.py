414ceba2a14073b8d1a13d4b73f35b69d7d03e1a
Hongyi <wangwhyh@gmail.com>
2021-11-23 23:54:06 -0800

    fix

--- backend/HQ/urls.py
+++ backend/HQ/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='HQ-home'),
+    path('manage/', views.home, name='HQ-home'),
     path('about/', views.about, name='HQ-about'),
 ]
